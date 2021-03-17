8a96412b58716081a51f76ada0ee457ed24d4578
Tin Marusic <marusischt@gmail.com>
2021-03-17 23:41:41 +0100

    kinematika

---
+++ vjezba_2-numericks_rjesenja/kinematika_2.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+from math import pi
+from zad_1_1 import jednoliko_gibanje
+from zad_2_1 import kosi_hitac
+
+jednoliko_gibanje(20,15)
+kosi_hitac(40,40)
