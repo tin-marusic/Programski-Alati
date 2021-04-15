e7f7f19fcdb080c9a4d611d8885af5d27959199b
Tin Marusic <marusischt@gmail.com>
2021-04-15 11:17:25 +0200

    funkcionira

--- domaci_01/provjera.py
+++ domaci_01/provjera.py
@@ -1,18 +1,18 @@
 import zadatak_01 as prt
 
 p1 = prt.particle(10,60,40,30)
-print(p1.total_time())
+#print(p1.total_time())
 p1.reset()
 
-p1 = prt.particle(10,60,40,100)
-print(p1.max_speed())
+p1 = prt.particle(10,0,40,100)
+#print(p1.max_speed())
 p1.reset()
 
-p1 = prt.particle(10,40,0,0)
-print(p1.velocity_to_hit_target(25,15,1))
+p1 = prt.particle(10,45,0,0)
+print(p1.velocity_to_hit_target(12,10,2))
 p1.reset()
 
-p1 = prt.particle(40,40,0,0)
-print(p1.angle_to_hit_target(25,15,0.37))
+p1 = prt.particle(25,40,0,0)
+print(p1.angle_to_hit_target(12,10,2))
 p1.reset()
 
