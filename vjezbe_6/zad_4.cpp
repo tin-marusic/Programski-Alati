9b261824c0acd073a90cbf5495ad663dc7aef72c
Tin Marusic <marusischt@gmail.com>
2021-05-02 23:15:32 +0200

    zadatak 4

--- vjezbe_6/zad_4.cpp
+++ vjezbe_6/zad_4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void dvije_jednadzbe(float a1, float b1, float c1, float a2, float b2, float c2){
     float y = (c2 * a1 - a2 * c1) / (-a2 * b1 + a1 * b2);
     float x = (c1 - b1*y) / a1;
-    std::cout << "x: " << x << "\ny: " << y <<std::endl;
+    cout << "x: " << x << "\ny: " << y <<endl;
 };
 
 int main(){
