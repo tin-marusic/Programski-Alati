b502175d77fbf91eb5faed057b63c3dedff73de2
Tin Marusic <marusischt@gmail.com>
2021-05-20 01:09:38 +0200

    organizacija

--- vjezbe_10/zad_1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import electromagnetic_field as em
-import matplotlib.pyplot as plt
-
-p2 = em.Electromagnetic(1,0,0,0,1,np.array([0.1,0.1,0.1]),np.array([0,0,1]),np.array([0,0,0]))
-p2.plot()
-p2.reset()
-
-
